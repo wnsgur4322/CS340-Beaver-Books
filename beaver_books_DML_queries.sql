a1433c55c257d12b7f86670507108088cb205db9
Junhyeok Jeong <jeongju@flip2.engr.oregonstate.edu>
2020-03-16 01:07:58 -0700

    complete

--- beaver_books_DML_queries.sql
+++ beaver_books_DML_queries.sql
@@ -1,7 +1,7 @@
 -- Name: Junhyeok (Derek) Jeong and Joelle Perez
--- Date: February 24, 2020
+-- Date: March 14, 2020
 -- Class: CS340-400
--- Program: Project Step 4 Draft Version (DML)
+-- Program: Project Step 7 (DML)
 
 --Variables are denoted by :
 
@@ -12,51 +12,95 @@
 -- ADMIN PAGE: 
 
 -- view books
-SELECT * FROM `books`;
+SELECT books.isbn, books.title, books.price, authors.first_name, authors.last_name, publishers.company_name, books.year FROM books INNER JOIN books_authors ON books.isbn = books_authors.isbn INNER JOIN authors ON books_authors.author_id = authors.author_id INNER JOIN publishers ON books.publisher_id = publishers.publisher_id;
 
 -- view users
 SELECT * FROM `users`;
 
--- add books
-INSERT INTO `books` (`isbn`, `author_id`, `title`, `price`, `publisher_id`, `year`)
-VALUES (:in_isbn, :in_authorID, :in_title, :in_price, :in_publisherID, :in_year);
+-- view authors
+SELECT * FROM `authors`;
+
+-- view publishers
+SELECT * FROM `publishers`;
+
+-- add new books
+-- 'GET' method
+query1: SELECT author_id, first_name, last_name from authors
+SELECT publisher_id, company_name from publishers
+query2: SELECT publisher_id, company_name from publishers
+query3: SELECT isbn from books
+
+-- 'POST' method
+qeury1: INSERT INTO `books` (`isbn`, `title`, `price`, `publisher_id`, `year`, `book_img`)
+VALUES (:in_isbn, :in_title, :in_price, :in_publisherID, :in_year, :in_book_img);
+query2: INSERT INTO `books_authors` (`isbn`, `author_id`) VALUES (:in_isbn, :in_athor_id)
+
+-- add new authors
+-- 'GET' method
+SELECT * FROM `authors`
+-- 'POST' method
+INSERT INTO `authors` (`first_name`, `last_name`, `address`, `url`) VALUES (:in_first_name, :in_last_name, :in_address, :in_url)
+
+-- add new publishers
+-- 'GET' method
+SELECT * from `publishers`
+-- 'POST' method
+INSERT INTO `publishers` (`company_name`, `contact`, `address`, `url`) VALUES (:in_company_name, :in_contact, :in_address, :in_url)
 
 -- delete books according to isbn
 DELETE FROM `books` WHERE `isbn` = :in_isbn;
 
--- updates when new user registers
-UPDATE `users` SET `email`= :in_email, `password` = :in_pass;
-
 -- delete users according to email
-DELETE FROM `users` WHERE `email` = :in_email;
+DELETE FROM `users` WHERE `user_id` = :in_user_id;
 
 -- when new book with new author is added
 INSERT INTO `books_authors` (`isbn`, `author_id`)
 VALUES (:in_isbn, :in_authorID);
 
--- deleting author which deletes there books
-DELETE FROM `books_authors` WHERE `isbn` = :in_isbn AND `author_id` = :in_authorID;
+-- book update
+-- 'GET' method
+SELECT * FROM `books` WHERE `isbn` = :in_isbn
+-- 'POST' method
+UPDATE `books` SET `price` = :in_price, `year` = :in_year, `book_img` = :in_book_img WHERE `isbn` = :in_isbn
 
--- deleting existing books which deletes books in shopping carts
-DELETE FROM `books_shopping_carts` WHERE `isbn` = :in_isbn AND `order_id` = :in_orderID;
 
 -- -- -- -- -- --
 
 -- LOGIN/REGISTER PAGES:
 
--- inserting login info/adding new user
-INSERT INTO `users` (`email`, `password`)
-VALUES (:in_email, :in_pass);
+-- login with existed account
+query1: SELECT `email`, `password` FROM `users`;
+query2: SELECT `user_id` FROM `users`;
+
+-- adding new user
+-- 'GET' method
+SELECT `user_id` FROM `users`;
+-- 'POST' method
+INSERT INTO `users` (`email`, `first_name`, `last_name`, `address`, `password`) VALUES (:in_email, :in_first_name, :in_last_name, :in_address, :in_password)
+
+-- -- -- -- -- --
+
+-- ACOUNT UPADATE:
+-- 'GET' method
+SELECT * FROM `users` WHERE `user_id` = :in_user_id;
+-- 'POST' method
+UPDATE `users` SET `email` = :in_email, `password` = :in_password, `address` = :in_address WHERE `user_id` = :in_user_id
 
 -- -- -- -- -- --
 
 -- SHOP PAGE:
 
--- searching available books by authors
-SELECT `first_name`, `last_name` FROM `authors`;
+-- searching available books by title
+SELECT `books.title`, `books.isbn`, `authors.first_name`, `authors.last_name`, `publishers.company_name`, `books.year`, `books.price`, `books.book_img` 
+	FROM `books` INNER JOIN `books_authors` ON `books.isbn` = `books_authors.isbn` INNER JOIN `authors` ON `books_authors.author_id` = `authors.author_id`
+	INNER JOIN `publishers` ON `books.publisher_id` = `publishers.publisher_id` WHERE title LIKE "%:in_books_title%";
+
 
 -- searching for available books by books
-SELECT `title` FROM `books`;
+SELECT `books.title`, `books.isbn`, `authors.first_name`, `authors.last_name`, `publishers.company_name`, `books.year`, `books.price`, `books.book_img`
+	FROM `books` INNER JOIN `books_authors` ON `books.isbn` = `books_authors.isbn` INNER JOIN `authors` ON `books_authors.author_id` = `authors.author_id`
+	INNER JOIN `publishers` ON `books.publisher_id` = `publishers.publisher_id`;
+
 
 -- adding new book to shopping cart
 INSERT INTO `shopping_carts` (`date`)
@@ -67,9 +111,9 @@ VALUES (:in_date);
 -- SHOPPING CART PAGE:
 
 -- deletes removed books
-DELETE FROM `shopping_carts` WHERE `order_id` = :in_orderID;
+DELETE FROM `shopping_carts` WHERE `date` = :in_date;
 
 -- updates when a new order is placed/removed
-UPDATE `shopping_carts` SET `order_id` = :in_orderID, `count` = :in_count;
+INSERT INTO `shopping_carts` (`user_id`, `isbn`) VALUES (:in_user_id, :in_isbn);
 
 -- -- -- -- -- --
